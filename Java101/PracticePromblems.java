0bc7c68cde933b00c0501d58749b6810e8834666
Jonah Gomez <jonahgomez690@gmail.com>
2024-11-18 21:32:24 -0500

    I solved practice problem 1

---
+++ Java101/PracticePromblems.java
@@ -0,0 +1,20 @@
+public class PracticePromblems {
+    public static int PracticePromblems_countOccurrences (int[] arr) {
+        int numOfn = 0;
+        for (int n : arr ) {
+            numOfn ++;
+        }
+        return numOfn;
+        
+    }
+
+    
+
+    public static void main(String[] args) {
+        
+        
+        
+    
+        
+    }
+}
