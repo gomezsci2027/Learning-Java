ab6614d635fa547731d6e1cb9c3d5c7359046056
Jonah Gomez <jonahgomez690@gmail.com>
2024-11-21 22:30:30 -0500

    solved practice problems 2 and 3

---
+++ Java101/PracticeProblems.java
@@ -0,0 +1,64 @@
+//problem 1
+public class PracticeProblems {
+    public static void main(String[] args) {
+        int[] arr= {1,3,6,4,4,3,6,7,4};
+        int n=4;
+
+        System.out.println(countOccurrences(arr,n));
+
+    }
+
+        public static int countOccurrences (int[] arr, int n) {
+            int numOfn = 0;
+            for (int number : arr ) {
+                if (number == n)
+                    numOfn ++;
+            }
+            return numOfn;
+            
+        }   
+}
+
+//problem 2
+public class PracticeProblems {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 7, 4, 2, 4, 6, 7, 3};
+        int[] reverseArr = reverseArray(arr);
+        for (int num : reverseArr) {
+            System.out.println(num);
+        }
+    }
+
+    public static int[] reverseArray(int[] arr) {
+        int[] reverse = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reverse[i] = arr[arr.length - 1 - i];
+        }
+        return reverse;
+    }
+}
+
+//problem 3
+public class PracticeProblems {
+    public static void main(String[] args) {
+        double[][] grid = {
+            {7,  3,  4}, 
+            {9,  2,  6}, 
+            {4,  1,  2}
+        };
+
+        double total = sumGrid(grid);
+        System.out.println(total);
+    }
+
+    public static double sumGrid(double[][] grid) {
+        double sum = 0.0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                sum += grid[i][j];
+            }
+        }
+
+        return sum;
+    }
+}
