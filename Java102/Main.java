c0ab015656f03a430633cc7a98d392813a56ba0f
Jonah Gomez <jonahgomez690@gmail.com>
2024-11-29 18:32:21 -0500

    Finished practice problem 1

---
+++ Java102/Main.java
@@ -0,0 +1,39 @@
+package Java102;
+
+
+class LibraryItem {
+    private String title;
+
+    public LibraryItem(String title) {
+        this.title = title;
+    }
+
+    public void returnItem() {
+        System.out.println(title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        LibraryItem book1 = new LibraryItem("The Kite Runner");
+        LibraryItem book2 = new LibraryItem("A Tree Grows in Brooklyn");
+        LibraryItem book3 = new LibraryItem("Moby Dick");
+        
+        LibraryItem[] items = {book1, book2, book3};
+        
+        returnAll(items);
+    }
+
+    public static void returnAll(LibraryItem[] items) {
+        for (LibraryItem item : items) {
+            item.returnItem();
+        }
+    }
+}
+
+
