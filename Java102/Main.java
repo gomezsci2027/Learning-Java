4629b3563f6e7abd31be3754c866c5e49205c349
Jonah Gomez <jonahgomez690@gmail.com>
2024-11-29 18:51:36 -0500

    Finished pracitce problem 2

--- Java102/Main.java
+++ Java102/Main.java
@@ -1,5 +1,4 @@
-package Java102;
-
+//Practice Problem 1
 
 class LibraryItem {
     private String title;
@@ -36,4 +35,64 @@ public class Main {
     }
 }
 
+//Practice Problem 2
 
+import java.util.ArrayList;
+
+class LibraryItem {
+    private String title;
+    private boolean available;  
+
+    
+    public LibraryItem(String title, boolean available) {
+        this.title = title;
+        this.available = available;
+    }
+
+    
+    public String getTitle() {
+        return title;
+    }
+
+    
+    public boolean isAvailable() {
+        return available;
+    }
+
+   
+    public void returnItem() {
+        System.out.println("Returning: " + title);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        LibraryItem book1 = new LibraryItem("The Great Gatsby", true);
+        LibraryItem book2 = new LibraryItem("1984", false);
+        LibraryItem magazine = new LibraryItem("National Geographic", true);
+        LibraryItem book3 = new LibraryItem("Moby Dick", false);
+
+        LibraryItem[] items = {book1, book2, magazine, book3};
+
+        ArrayList<LibraryItem> availableItemsList = availableItems(items);
+
+        System.out.println("\nAvailable items:");
+        for (LibraryItem item : availableItemsList) {
+            System.out.println(item.getTitle());
+        }
+    }
+
+    public static ArrayList<LibraryItem> availableItems(LibraryItem[] items) {
+        ArrayList<LibraryItem> availableList = new ArrayList<>();  
+
+        for (LibraryItem item : items) {
+            
+            if (item.isAvailable()) {
+                availableList.add(item);
+            }
+        }
+
+        return availableList;  
+    }
+}
